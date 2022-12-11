f9955d3263722abfb6dfe7ccaf82797f324ad32f
SophIren <45230353+SophIren@users.noreply.github.com>
2022-12-11 09:21:42 +0300

    Very very gut commit

--- handlers/tree/tree_writer.py
+++ handlers/tree/tree_writer.py
@@ -4,8 +4,8 @@ from handlers.index_handler import IndexHandler
 
 
 class TreeWriteHandler(IndexHandler):
-    def drop_commited(self, commit_path: Path):
+    def drop_commited(self, commit_path: Path) -> None:
         pass
 
-    def add_commited(self, commit_path: Path):
+    def add_commited(self, commit_path: Path) -> None:
         pass
